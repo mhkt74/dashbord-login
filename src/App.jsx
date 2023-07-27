bffa6be4482cb96735be496da7850e0df65f6f0e
mohammad hosein kabiri <mh.kt.7495@gmail.com>
2023-07-27 13:40:31 +0330

    add routs

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import "./Css/App.css";
 import React from "react";
-
+import { BrowserRouter as Router, Route ,Routes } from "react-router-dom";
+import Main from "./Pages/Main";
+import AboutUs from "./Pages/AboutUs";
+import ContactUs from "./Pages/ContactUs";
 function App() {
   return (
-    <div className="App">
-      <p>سلام محمدحسین کبیری هستم.</p>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes path="/Main" element={<Main />}>
+          <Route path="about-us" element={<AboutUs />} />
+          <Route path="contact-us" element={<ContactUs/>} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- src/Pages/Main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Main() {
-  return (
-    <div className='Main'>Main</div>
-  )
-}
-
-export default Main
