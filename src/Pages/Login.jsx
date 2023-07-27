3d181106df5ef531a1ef5a22d6c2883e0b99d021
mohammad hosein kabiri <mh.kt.7495@gmail.com>
2023-07-27 18:10:26 +0330

    finish login page

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,14 +1,26 @@
 import React from "react";
 import "../Css/Login.css";
+import { Link } from "react-router-dom";
 
 function Login() {
   return (
     <div className="Login">
-      <span>محمدحسین کبیری هستم اومدیم با هم یه داشبورد داشته باشیم . </span>
-      <span> با ورود به صفحه امیدوارم لذت ببرید .</span>
+      <span className="name-god font-size-25">به نام او و برای او</span>
       <div className="box-login">
-        <input></input>
+        <p className="font-size-20">صفحه ورود</p>
+        <div className="input-box">
+          <label htmlFor="userName">نام کاربری</label>
+          <input type="text" id="userName"></input>
+        </div>
+        <div className="input-box">
+          <label htmlFor="password">رمزعبور</label>
+          <input type="password" id="password"></input>
+        </div>
+        <button className="button-login">ورود</button>
       </div>
+      <Link className="form_prev_button_item" to="/register">
+        ثبت نام کنید
+      </Link>
     </div>
   );
 }
