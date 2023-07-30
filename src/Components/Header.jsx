551a1cb3fca718590ca808f59f84e1b378567527
mohammad hosein kabiri <mh.kt.7495@gmail.com>
2023-07-30 07:27:51 -0700

    edit header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import "../Css/Header.css";
 import { Link } from "react-router-dom";
 import Avatar from "../Assets/image/SVG/Avatar";
 function Header() {
+
+  const [showMobileHeader, setShowMobileHeader] = useState(false)
+
+
   return (
     <div className="Header">
-      <div className="rigth-header">
+      <div className="rigth-header-des">
         <Link className="text-header" to="/main">
           خانه
         </Link>
@@ -19,6 +23,27 @@ function Header() {
           وبلاگ
         </Link>
       </div>
+
+      <div className="rigth-header-Mobile">
+        <div></div>
+      </div>
+        {
+          showMobileHeader ?
+            <div className="">
+              <Link className="text-header" to="/main">
+                خانه
+              </Link>
+              <Link className="text-header" to="about-us">
+                درباره ما
+              </Link>
+              <Link className="text-header" to="contact-us">
+                ارتباط با ما
+              </Link>
+              <Link className="text-header" to="#">
+                وبلاگ
+              </Link>
+            </div> : ""
+        }
       <div className="left-header">
         <p className="name-user">سلام کاربر</p>
         <div className="avatar">
