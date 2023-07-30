3841d8eecc49d40e9aab1502e41fc6886defbf5b
mohammad hosein kabiri <mh.kt.7495@gmail.com>
2023-07-31 00:24:31 +0330

    start header responsive

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,10 +3,6 @@ import "../Css/Header.css";
 import { Link } from "react-router-dom";
 import Avatar from "../Assets/image/SVG/Avatar";
 function Header() {
-
-  const [showMobileHeader, setShowMobileHeader] = useState(false)
-
-
   return (
     <div className="Header">
       <div className="rigth-header-des">
@@ -23,33 +19,53 @@ function Header() {
           وبلاگ
         </Link>
       </div>
-
       <div className="rigth-header-Mobile">
-        <div></div>
+        <nav>
+          <div class="navbar">
+            <div class="container nav-container">
+              <input class="checkbox" type="checkbox" name="" id="" />
+              <div class="hamburger-lines">
+                <span class="line line1"></span>
+                <span class="line line2"></span>
+                <span class="line line3"></span>
+              </div>
+              <div class="menu-items">
+                <li>
+                  <Link className="text-header" to="/main">
+                    خانه
+                  </Link>
+                </li>
+                <li>
+                  <Link className="text-header" to="about-us">
+                    درباره ما
+                  </Link>
+                </li>
+                <li>
+                  <Link className="text-header" to="contact-us">
+                    ارتباط با ما
+                  </Link>
+                </li>
+                <li>
+                  <Link className="text-header" to="#">
+                    وبلاگ
+                  </Link>
+                </li>
+              </div>
+            </div>
+          </div>
+        </nav>
       </div>
-        {
-          showMobileHeader ?
-            <div className="">
-              <Link className="text-header" to="/main">
-                خانه
-              </Link>
-              <Link className="text-header" to="about-us">
-                درباره ما
-              </Link>
-              <Link className="text-header" to="contact-us">
-                ارتباط با ما
-              </Link>
-              <Link className="text-header" to="#">
-                وبلاگ
-              </Link>
-            </div> : ""
-        }
+
+      {/* <div className="menu-items">
+      </div> */}
       <div className="left-header">
         <p className="name-user">سلام کاربر</p>
         <div className="avatar">
           <Avatar />
         </div>
-        <Link className="text-header" to="/">خروج</Link>
+        <Link className="text-header" to="/">
+          خروج
+        </Link>
       </div>
     </div>
   );
